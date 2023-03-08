a77792753ea49a323293b9bbc129c1bfe80d0907
Rachel Schipull <rlschipull@gmail.com>
2023-03-08 12:09:03 -0500

    fill viewport

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{height:'100vh'}}>
       <Header />
       <Cyclinks />
       <Footer />
